2414168107370176c3723e00eed393ffe9c0844a
SaulsCode <52906811+DaleSauls123@users.noreply.github.com>
2021-06-16 05:30:02 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,27 @@
+import java.text.NumberFormat;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.graalvm.compiler.nodes.NamedLocationIdentity;
+
+//import java.util.Date;
+
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        
+       Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int number = scanner.nextInt();
+
+        if (number % 5 == 0 && number % 3 == 0)
+        System.out.println("FizzBuzz");
+        else if (number % 5 == 0)
+            System.out.println("Fizz");
+        else if (number % 3 == 0)
+            System.out.println("Buzz");
+        else   
+            System.out.println(number); 
+    }
+}
